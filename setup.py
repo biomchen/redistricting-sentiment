e343ef3be41652a0e7c4157ab7cd7f5a41702c74
Meng Chen <meng.chen03@gmail.com>
2019-09-21 11:19:36 -0400

    Adding more details in setup file

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.txt')
 reqs = [str(each_req.req) for each_req in install_reqs]
 
-setup(name="redistrict",
-      version="0.1",
+setup(name='redistrict',
+      version='0.1',
       description='Simple API to analyze sentiments of LOU redistricting',
-      author="Meng Chen",
-      author_email="meng.chen03@gmail.com",
+      url='https://github.com/biomchen/2019_LOU_Redistricting_Sentiment',
+      author='Meng Chen',
+      author_email='meng.chen03@gmail.com',
+      license='MIT',
       install_requires=reqs)
