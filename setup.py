e8dc516f117aa6a0fa83668b4ff1329184e545b1
Meng Chen <meng.chen03@gmail.com>
2019-09-23 14:04:43 -0400

    Update setup.py
    Implementation of parse_requirments from pip cause confusion in the different versions of the pip.  By using the readlines(), there is no errors occuring during the dependency installation.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(each_req.req) for each_req in install_reqs]
+install_reqs = open('requirements.txt')
+lines = install_reqs.readlines()
+reqs = [str(each_req) for each_req in lines]
 
 setup(name='redistrict',
       version='0.1',
       description='Simple API to analyze sentiments of LOU redistricting',
-      url='https://github.com/biomchen/2019_LOU_Redistricting_Sentiment',
+      long_description = read_file('README.md'),
       author='Meng Chen',
       author_email='meng.chen03@gmail.com',
+      url='https://github.com/biomchen/2019_LOU_Redistricting_Sentiment',
       license='MIT',
-      install_requires=reqs)
+      install_requires=reqs,
+      keywords = 'sentiment Frederick Maryland redistricting')
